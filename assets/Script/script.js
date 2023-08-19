ec883b503f726b0d1b8a32b65df1e563405e196e
Cristian Hanganu <cristihell23@yahoo.com>
2023-08-19 15:13:49 +0000

    Added hints to word list + small changes

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,11 +1,218 @@
-//Global Variables //
+// Guess Game Variable
+let random = Math.floor(Math.random() * 10);
 let totalScoreSaver = parseInt(localStorage.getItem('totalScoreSaver'));
-console.log(totalScoreSaver);
-let words = ['display','fix','great','bad','asparagus','suspicious'];
-let word = Math.floor(Math.random() * words.length);
 let totalScore = document.getElementById('score');
 let mistake = 0;
-let random = Math.floor(Math.random() * 10);
+
+
+// Word Guess Game Variable
+let words= [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "ebay",
+        hint: "online shopping site"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "related to programming"
+    },
+    {
+        word: "avatar",
+        hint: "epic science fiction film"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "mental",
+        hint: "related to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to a indoor game"
+    },
+    {
+        word: "viber",
+        hint: "a social media app"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "nepal",
+        hint: "developing country name"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "a country name"
+    },
+    {
+        word: "joker",
+        hint: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+];
+let ind = Math.floor(Math.random() * words.length);
+let answer = '';
+
+
+for (letter in words[ind].word){
+    answer += '_ '
+};
+
+console.log(answer);
 console.log(random);
 
 // Guess Correct Number Game //
@@ -33,17 +240,14 @@ const submit = () => {
 
 //Word Guess Game //
 const wsubmit = () => {
-    let answer = '';
-    for (letter in words[word]){
-        if (document.getElementById ('wanswer').value === letter){
-            console.log(letter);
-        }
-        else{
-            console.log(words[word])
-        }
-    }
-}
-
+    for (letter in words[ind].word){
+        console.log(words[ind].word[letter]);
+        if (document.getElementById('wanswer').value === words[ind].word[letter]){
+            alert('This is correct');
+        };
+    };
+    alert(`Hint: ${words[ind].hint}`);
+};
 //Maths Game //
 
 

6828c3214166fca7b929af7bf8141f997d3554a7
Cristian Hanganu <cristihell23@yahoo.com>
2023-08-19 13:38:45 +0000

    numguess Update

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,10 +1,20 @@
-// Guess Correct Number Game //
+//Global Variables //
+let totalScoreSaver = parseInt(localStorage.getItem('totalScoreSaver'));
+console.log(totalScoreSaver);
+let totalScore = document.getElementById('score');
+let mistake = 0;
 let random = Math.floor(Math.random() * 10);
+console.log(random);
 
+// Guess Correct Number Game //
 const submit = () => {
     if (parseInt(document.getElementById ('answer').value) === random) {
         alert('This is correct');
         document.getElementById('num').textContent = `${random}`;
+        totalScoreSaver+=1;
+        console.log(totalScoreSaver);
+        localStorage.setItem("totalScoreSaver", (totalScoreSaver));
+        window.history.back();
     }
     else {
         let x = document.createElement("i");
@@ -12,6 +22,9 @@ const submit = () => {
         document.body.appendChild(x);
         document.getElementById('heart-wrapper').appendChild(x);
         alert('This is not the correct Number');
+        mistake+=1;
+        if (mistake === 3){
+            window.history.back();
+        }
     };
 };
-// Guess Correct Number Game //

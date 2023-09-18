35844d8c119c8881c7aeaf1a88511d270cc7430b
Cristian Hanganu <cristihell23@yahoo.com>
2023-09-18 19:45:08 +0000

    snake head spawn (bugged as it spawns inside food div)

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,16 +1,22 @@
 const area = document.querySelector('.area');
 
 let gameOver = false;
-let foodX =3 , foodY= 3;
+let foodX, foodY;
 let snakeBody = [];
 let score = 0;
 let snakeX = 7, snakeY =7;
 
-
+const randomFoodSpawner = () => {
+    // Random Coordinates to match Grid size
+    foodX = Math.floor(Math.random() * 30) +1;
+    foodY = Math.floor(Math.random() * 30) +1;
+}
 
 const startGame = () => {
     let food = `<div class ='food' style = 'grid-area: ${foodY} / ${foodX}'><div>`;
+    food += `<div class ='snakeHead' style = 'grid-area: ${snakeY} / ${snakeX}'><div>`;
     area.innerHTML = food;
 };
 
+randomFoodSpawner();
 startGame();
